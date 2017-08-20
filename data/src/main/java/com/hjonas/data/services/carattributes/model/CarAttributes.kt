097ceaedaf81ea4782c08a80469d6802c2d260ef
Jonas Hansson <jonas.hansson@magine.com>
2017-08-20 14:20:58 +0200

    added parcelable for data model

--- data/src/main/java/com/hjonas/data/services/carattributes/model/CarAttributes.kt
+++ data/src/main/java/com/hjonas/data/services/carattributes/model/CarAttributes.kt
@@ -1,7 +1,10 @@
 package com.hjonas.data.services.carattributes.model
 
 import com.google.gson.annotations.SerializedName
+import paperparcel.PaperParcel
+import paperparcel.PaperParcelable
 
+@PaperParcel
 data class CarAttributes(val regno: String
                          , val vin: String
                          , val brand: String
@@ -9,23 +12,52 @@ data class CarAttributes(val regno: String
                          , @SerializedName("gearbox_type") val gearboxType: String
                          , @SerializedName("fuel_types") val fuelTypes: List<String>
                          , val emission: Emission?
-                         , val fuel: Fuel?)
+                         , val fuel: Fuel?) : PaperParcelable {
+    companion object {
+        @JvmField val CREATOR = PaperParcelCarAttributes.CREATOR
+    }
+}
 
 // Data classes for emissions information
+@PaperParcel
 data class Emission(val gasoline: FuelEmission?
-                    , val diesel: FuelEmission?) {
-    data class FuelEmission(val co2: DrivingValues)
+                    , val diesel: FuelEmission?) : PaperParcelable {
+    companion object {
+        @JvmField val CREATOR = PaperParcelEmission.CREATOR
+    }
+
+    @PaperParcel
+    data class FuelEmission(val co2: DrivingValues) : PaperParcelable {
+        companion object {
+            @JvmField val CREATOR = PaperParcelEmission_FuelEmission.CREATOR
+        }
+    }
 }
 
 // Data classes for fuel information
+@PaperParcel
 data class Fuel(val gasoline: FuelType?
-                , val diesel: FuelType?) {
-    data class FuelType(@SerializedName("average_consumption") val averageConsumption: DrivingValues)
+                , val diesel: FuelType?) : PaperParcelable {
+    companion object {
+        @JvmField val CREATOR = PaperParcelFuel.CREATOR
+    }
+
+    @PaperParcel
+    data class FuelType(@SerializedName("average_consumption") val averageConsumption: DrivingValues) : PaperParcelable {
+        companion object {
+            @JvmField val CREATOR = PaperParcelFuel_FuelType.CREATOR
+        }
+    }
 }
 
+@PaperParcel
 data class DrivingValues(val urban: Double?
                          , val mixed: Double?
-                         , val rural: Double?)
+                         , val rural: Double?) : PaperParcelable {
+    companion object {
+        @JvmField val CREATOR = PaperParcelDrivingValues.CREATOR
+    }
+}
 
 object Brands {
     const val Volvo = "volvo"
