2f3e707d5179fb5b7a7c96070c27acf604fb72ee
Jonas Hansson <jonas.hansson@springworks.se>
2018-07-06 10:14:10 +0200

    updated tests

--- app/src/test/java/com/hjonas/carattr/ui/presenter/CarAttributesPresenterTest.kt
+++ app/src/test/java/com/hjonas/carattr/ui/presenter/CarAttributesPresenterTest.kt
@@ -4,22 +4,29 @@ import com.hjonas.carattr.ui.CarAttributesContract
 import com.hjonas.carattr.ui.attributeslist.*
 import com.hjonas.data.services.carattributes.CarAttributesService
 import com.hjonas.data.services.carattributes.model.CarAttributes
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
+import com.hjonas.data.services.carattributes.model.DrivingValues
+import com.hjonas.data.services.carattributes.model.Emission
+import com.hjonas.data.services.carattributes.model.Fuel
+import com.nhaarman.mockito_kotlin.*
 import io.reactivex.Observable
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.schedulers.Schedulers
 import org.junit.Before
+import org.junit.Test
+import retrofit2.HttpException
 
 class CarAttributesPresenterTest {
 
   private val TEST_CAR_ATTRIBUTES by lazy {
+    val emission = Emission(gasoline = null, diesel = Emission.FuelEmission(DrivingValues(urban = 12.3, mixed = 45.6, rural = 78.9)))
+    val fuel = Fuel(gasoline = Fuel.FuelType(DrivingValues(urban = 12.3, mixed = 45.6, rural = 78.9)), diesel = null)
     CarAttributes("ABC123",
                   "VIN",
                   "Volvo",
                   2017,
                   "Manual",
                   listOf("Gasoline", "Diesel"),
-                  null, null)
+                  emission, fuel)
   }
   private val TEST_SECTIONS by lazy {
     listOf(CarAttributeItem(AttributeSectionType.FUEL_CONSUMPTION, TEST_CAR_ATTRIBUTES, Gasoline()),
@@ -32,11 +39,39 @@ class CarAttributesPresenterTest {
   private lateinit var api: CarAttributesService
 
   @Before
-  private fun setUp() {
+  fun setUp() {
+    RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+
     view = mock()
     sectionsProvider = mock { on { getSections(any()) } doReturn TEST_SECTIONS }
     api = mock { on { fetchCarAttributes() } doReturn Observable.just(TEST_CAR_ATTRIBUTES) }
 
     presenter = CarAttributesPresenter(view, "vin", sectionsProvider, api)
+  }
+
+  @Test
+  fun testFetchAttributesSuccessful() {
+    whenever(api.fetchCarAttributes()).thenReturn(Observable.just(TEST_CAR_ATTRIBUTES))
+
+    presenter.subscribe()
+
+    verify(view).showLoading()
+    verify(view).hideLoading()
+    verify(sectionsProvider).getSections(eq(TEST_CAR_ATTRIBUTES))
+    verify(view).showAttributeSections(eq(TEST_SECTIONS))
+  }
+
+  @Test
+  fun testFetchAttributesFailed() {
+    val exception: HttpException = mock {
+      on { code() } doReturn 401
+    }
+    whenever(api.fetchCarAttributes()).thenReturn(Observable.error(exception))
+
+    presenter.subscribe()
+
+    verify(view).showLoading()
+    verify(view).hideLoading()
+    verify(view).showIncorrectResponseError(401)
   }
 }
--- data/src/main/java/com/hjonas/data/services/carattributes/model/CarAttributes.kt
+++ data/src/main/java/com/hjonas/data/services/carattributes/model/CarAttributes.kt
@@ -1,10 +1,8 @@
 package com.hjonas.data.services.carattributes.model
 
 import com.google.gson.annotations.SerializedName
-import paperparcel.PaperParcel
-import paperparcel.PaperParcelable
 
-@PaperParcel
+
 data class CarAttributes(val regno: String
                          , val vin: String
                          , val brand: String
@@ -12,58 +10,24 @@ data class CarAttributes(val regno: String
                          , @SerializedName("gearbox_type") val gearboxType: String
                          , @SerializedName("fuel_types") val fuelTypes: List<String>
                          , val emission: Emission?
-                         , val fuel: Fuel?) : PaperParcelable {
-  companion object {
-    @JvmField
-    val CREATOR = PaperParcelCarAttributes.CREATOR
-  }
-}
+                         , val fuel: Fuel?)
 
 // Data classes for emissions information
-@PaperParcel
 data class Emission(val gasoline: FuelEmission?
-                    , val diesel: FuelEmission?) : PaperParcelable {
-  companion object {
-    @JvmField
-    val CREATOR = PaperParcelEmission.CREATOR
-  }
-
-  @PaperParcel
-  data class FuelEmission(val co2: DrivingValues) : PaperParcelable {
-    companion object {
-      @JvmField
-      val CREATOR = PaperParcelEmission_FuelEmission.CREATOR
-    }
-  }
+                    , val diesel: FuelEmission?) {
+  data class FuelEmission(val co2: DrivingValues)
 }
 
 // Data classes for fuel information
-@PaperParcel
 data class Fuel(val gasoline: FuelType?
-                , val diesel: FuelType?) : PaperParcelable {
-  companion object {
-    @JvmField
-    val CREATOR = PaperParcelFuel.CREATOR
-  }
+                , val diesel: FuelType?) {
 
-  @PaperParcel
-  data class FuelType(@SerializedName("average_consumption") val averageConsumption: DrivingValues) : PaperParcelable {
-    companion object {
-      @JvmField
-      val CREATOR = PaperParcelFuel_FuelType.CREATOR
-    }
-  }
+  data class FuelType(@SerializedName("average_consumption") val averageConsumption: DrivingValues)
 }
 
-@PaperParcel
 data class DrivingValues(val urban: Double?
                          , val mixed: Double?
-                         , val rural: Double?) : PaperParcelable {
-  companion object {
-    @JvmField
-    val CREATOR = PaperParcelDrivingValues.CREATOR
-  }
-}
+                         , val rural: Double?)
 
 /**
  * Returns true if this object has at least one child that is non-null, otherwise false
